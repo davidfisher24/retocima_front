2fa20c1cff3b5545f038052b399c6669db154554
David <davidfishe24@gmail.com>
2018-04-08 07:52:29 +0200

    Blurb changes

--- src/components/Blurb.vue
+++ src/components/Blurb.vue
@@ -1,13 +1,15 @@
 <template>
   <div>
-	  <p class="headline display-2">
+	  <p class="headline">
 	  <span class="accent--text">C</span>ertificado 
 	  <span class="accent--text">I</span>bérico de 
 	  <span class="accent--text">M</span>ontañas 
 	  <span class="accent--text">A</span>scendidas</p>
+	  <p class="subheading">Los mejores puertos de montaña de España, Andorra y Portugal</p>
 	  <p class="subheading">¿Qué es el C.I.M.A.?</p>
 	  <p class="body-2">El CIMA puede considerarse como un reto personal de carácter lúdico y no competitivo, para todos aquellos amantes del cicloturismo que deseen disfrutar ascendiendo los puertos más representativos de cada provincia española, así como Andorra y Portugal.</p>
 	  <p class="body-2">Todas estas cumbres han sido debatidas y seleccionadas por representantes de las diferentes regiones de nuestra geografía para, tras varios años de ardua labor y discusión en el foro de www.altimetrias.com, consensuar una lista de 640 ascensiones en las que se busca no sólo dureza, también belleza e historia, dentro de las posibilidades orográficas de cada región.</p>
 	  <p class="body-2">Descubre, pues, los principales puertos de montaña ibéricos, y lánzate a alcanzar el máximo número de ascensiones</p>
+	  <p class="subheading text-xs-left"><strong>Serás capaz de conseguirlas todas? Date de alta y !!<span class="accent--text">Empieza ya</span>!!</strong></p>
 	</div>
 </template>
