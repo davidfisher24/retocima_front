915445a3dae2e86f7e80b5bbcef890a55b08a36a
David <davidfishe24@gmail.com>
2018-10-04 18:24:09 +0200

    Development advanced search component

--- src/components/Search/AdvancedSearch.vue
+++ src/components/Search/AdvancedSearch.vue
@@ -3,57 +3,47 @@
     <v-layout row wrap>
       <v-flex xs12 md6>
         <div class="mt-5">
-         <vue-slider ref="slider" v-model="value" :min="min" :max="max"
-         :bg-style="{}"
-         :slider-style="{background:$vuetify.theme.accent}"
-         :process-style="{background:$vuetify.theme.accent}"
-         :tooltip-style="{border:'none', background:'none',color:$vuetify.theme.primary, fontSize:'18px'}"
-         :label-style="{}"
-         width="90%"
-         ></vue-slider>
+
+          <DoubleSlider
+            minimum="0"
+            maximum="40"
+            title="Longitud"
+            units="km"
+            ref="km"
+          ></DoubleSlider>
+
+          <DoubleSlider
+            minimum="0"
+            maximum="320"
+            title="APM"
+            ref="apm"
+          ></DoubleSlider>
+
+          <v-btn @click="search">Submit</v-btn>
+
          </div>
       </v-flex>
     </v-layout>
   </v-container> 
 </template>
 
-<style>
-
-
-  .vue-slider-tooltip: {
-      margin-bottom: -10px;
-  }
-
-  .vue-slider-tooltip::before {
-    border:none;
-  }
-</style>
-
 <script>
 
-import vueSlider from 'vue-slider-component'
+import DoubleSlider from './DoubleSlider'
 
 export default {
   data () {
     return {
     
-        value: [
-          0,
-          2000
-        ],
-        min: 0,
-        max: 2000,
-
     }
   },
 
   components: {
-    vueSlider
+    DoubleSlider
   },
   
   mounted () {
-    console.log(this)
-    console.log(this.$vuetify.theme.accent)
+
   },
 
   watch: {
@@ -62,6 +52,10 @@ export default {
 
   methods: {
 
+    search() {
+      alert("Kilometres btw " + this.$refs.km.value[0] + " & " + this.$refs.km.value[1])
+      alert("APM btw " + this.$refs.apm.value[0] + " & " + this.$refs.apm.value[1])
+    }
 
   }
 }
---
+++ src/components/Search/DoubleSlider.vue
@@ -0,0 +1,57 @@
+<template>
+   <v-layout wrap grid>
+    <v-flex xs12 class="mb-3 pl-4 subheading primary--text">
+     <p>
+        <strong>{{title}}</strong>
+        <span v-if="units"><i>{{units}}</i></span>
+      </p>
+    </v-flex>
+    <v-flex xs12>
+     <vue-slider ref="slider" v-model="value" :min="min" :max="max"
+     :bg-style="{}"
+     :slider-style="{background:$vuetify.theme.accent}"
+     :process-style="{background:$vuetify.theme.accent}"
+     :tooltip-style="{border:'none', background:'none',color:$vuetify.theme.primary, fontSize:'18px'}"
+     :label-style="{}"
+     width="90%"
+     ></vue-slider>
+    </v-flex>
+  </v-layout>
+</template>
+
+<style>
+  .vue-slider-tooltip: {
+      margin-bottom: -10px;
+  }
+  .vue-slider-tooltip::before {
+    border:none;
+  }
+</style>
+
+<script>
+
+import vueSlider from 'vue-slider-component'
+
+export default {
+  props: ["minimum","maximum", "title", "units"],
+
+  components: {
+    vueSlider
+  },
+  
+  beforeMount () {
+    this.min = parseInt(this.minimum);
+    this.max = parseInt(this.maximum);
+    this.value = [this.min,this.max];
+  },
+
+  watch: {
+
+  },
+
+  methods: {
+
+
+  }
+}
+</script>
